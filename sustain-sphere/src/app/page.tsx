d44ee0b474c6e6d898d9ce876af655f60fce2d34
arpittiwari24 <100207940+arpittiwari24@users.noreply.github.com>
2023-09-02 21:16:05 +0530

    made changes in the next.config.js and page.tsx

--- sustain-sphere/src/app/page.tsx
+++ sustain-sphere/src/app/page.tsx
@@ -1,13 +1,16 @@
-import Image from 'next/image'
+// import Image from 'next/image'
 import {options} from "@/app/api/auth/[...nextauth]/options";
 import {getServerSession} from "next-auth";
 import UserCard from "./components/UserCard"
 
 export default async function Home() {
   const session = await getServerSession(options)
+  console.log(session)
+
   return (
-      <main className="flex min-h-screen flex-col items-center justify-between p-24">
-          hello
+      <main className="bg-white text-black flex min-h-screen flex-col items-center justify-between p-24">
+          hello world
+          <UserCard user={session?.user} pagetype="Home" />
       </main>
   )
 }
