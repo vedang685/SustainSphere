504b778569c056b0f083f62e39eece55254efba3
Ashish Yadav <48384865+criticalAY@users.noreply.github.com>
2023-09-08 03:48:33 +0530

    Hero Section

---
+++ sustain-sphere/src/app/components/ui/SSFeatures.tsx
@@ -0,0 +1,40 @@
+import exp from "constants";
+
+
+const Features= () =>{
+    return(
+
+        <div className="max-w-6xl px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto">
+            <div
+                className="min-h-[35vh] bg-[url('https://images.unsplash.com/photo-1665686374006-b8f04cf62d57?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80')] bg-center bg-cover bg-no-repeat relative rounded-xl md:min-h-[75vh]">
+                <div
+                    className="absolute bottom-0 left-0 right-0 max-w-xs text-center mx-auto p-6 md:left-auto md:text-left md:mx-0">
+                    {/*// <!-- Card -->*/}
+                    <div className=" inline-block bg-white rounded-lg md:p-7 dark:bg-gray-800">
+                        <div className="hidden md:block">
+                            <h3 className="text-lg font-bold text-gray-800 sm:text-2xl dark:text-gray-200">How does Preline
+                                work?</h3>
+                            <p className="mt-2 text-gray-800 dark:text-gray-200">Learn more about Preline.</p>
+                        </div>
+
+                        <div className="md:mt-16">
+                            <a className="flex items-center gap-2 text-sm font-medium text-gray-800 hover:text-gray-500 dark:text-white dark:hover:text-gray-400"
+                               href="#">
+                                <svg className="w-4 h-auto" xmlns="http://www.w3.org/2000/svg" width="16" height="16"
+                                     fill="currentColor" viewBox="0 0 16 16">
+                                    <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+                                    <path
+                                        d="M6.271 5.055a.5.5 0 0 1 .52.038l3.5 2.5a.5.5 0 0 1 0 .814l-3.5 2.5A.5.5 0 0 1 6 10.5v-5a.5.5 0 0 1 .271-.445z"/>
+                                </svg>
+                                Watch our story
+                            </a>
+                        </div>
+                    </div>
+                    {/*// <!-- End Card -->*/}
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Features;
