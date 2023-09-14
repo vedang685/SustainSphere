0a371b8e22c5334d61d42ef5531df62af6ad1b40
Vedang <vedang685@gmail.com>
2023-09-14 23:16:18 +0530

    added revit pages and made responsive changes

---
+++ sustain-sphere/src/app/(webpages)/revit/oceanacidification/page.tsx
@@ -0,0 +1,68 @@
+import Blogbg from "../blogbg"
+import { Poppins, Lora} from "@next/font/google"
+
+const pops = Poppins({
+    weight: ["500"],
+    style: ["normal"],
+    subsets: ["latin"]
+})
+const lora = Lora({
+    weight: ["400"],
+    style: ["normal"],
+    subsets: ["latin"]
+})
+
+export default function Deforestation(){
+    return(
+        <div className="mx-3">
+            <div className="relative">
+                <div className="rounded-2xl border-2 border-green-700 w-full mt-6 mb-10 bg-black">
+                    <img className="w-full rounded-2xl opacity-[70%] " src="/images/assets/revit/oceanAcid.png"/>
+                </div>
+                <div className="absolute w-full top-[40%] left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center">
+                    <p style={pops.style} className=" text-4xl">Soil Erosion</p>
+                </div>
+                <div className="absolute sm:bottom-[5%] md:bottom-[10%] lg:bottom-[25%] xl:bottom-[38%] left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center">
+                    <p style={pops.style} className="text-white leading-6 text-xs">Explore a premier online platform for cutting-edge environmental management solutions. Discover a wealth of resources, tools, and insights to assist your organization in achieving excellence in sustainability and environmental stewardship.</p>
+                </div>
+            </div>
+            <div className="w-full relative overflow-hidden">
+                <div style={lora.style} className="w-[70%] leading-7 ml-[15%] mb-10 text-[#495057]">
+                    <p style={lora.style} className="text-2xl text-black mb-8">
+                        Soil Erosion
+                    </p>
+                        Sed lectus vestibulum mattis ullamcorper velit. Non consectetur a erat nam at. At quis risus sed vulputate. Dui ut ornare lectus sit amet est. Urna porttitor rhoncus dolor purus non enim praesent elementum. Et netus et malesuada fames ac turpis egestas maecenas. Nibh venenatis cras sed felis. Vitae tortor condimentum lacinia quis vel eros donec ac. Morbi tempus iaculis urna id volutpat lacus laoreet. Turpis cursus in hac habitasse platea dictumst. Pellentesque id nibh tortor id aliquet lectus proin nibh nisl. Diam quam nulla porttitor massa. Malesuada proin libero nunc consequat interdum varius sit amet. Nisl purus in mollis nunc sed. Sollicitudin ac orci phasellus egestas tellus rutrum tellus. Dolor sit amet consectetur adipiscing elit ut aliquam purus. Quis lectus nulla at volutpat diam ut venenatis. Nibh cras pulvinar mattis nunc sed blandit libero volutpat.
+                        Sed adipiscing diam donec adipiscing tristique risus nec feugiat. Eu tincidunt tortor aliquam nulla facilisi cras. Sit amet luctus venenatis lectus magna. Neque gravida in fermentum et sollicitudin ac orci. Pharetra sit amet aliquam id diam. Integer malesuada nunc vel risus. Egestas erat imperdiet sed euismod nisi. Eget aliquet nibh praesent tristique magna sit amet purus gravida. Pretium quam vulputate dignissim suspendisse. Feugiat in fermentum posuere urna nec. Ut sem viverra aliquet eget sit amet.
+                        Etiam dignissim diam quis enim lobortis. Integer malesuada nunc vel risus commodo viverra. Massa tincidunt nunc pulvinar sapien 
+                    <p style={lora.style} className="text-2xl text-black mt-8 mb-8">
+                        Solutions
+                    </p>
+                    Sed lectus vestibulum mattis ullamcorper velit. Non consectetur a erat nam at. At quis risus sed vulputate. Dui ut ornare lectus sit amet est. Urna porttitor rhoncus dolor purus non enim praesent elementum. Et netus et malesuada fames ac turpis egestas maecenas. Nibh venenatis cras sed felis. Vitae tortor condimentum lacinia quis vel eros donec ac. Morbi tempus iaculis urna id volutpat lacus laoreet. Turpis cursus in hac habitasse platea dictumst. Pellentesque id nibh tortor id aliquet lectus proin nibh nisl. Diam quam nulla porttitor massa. Malesuada proin libero nunc consequat interdum varius sit amet. Nisl purus in mollis nunc sed. Sollicitudin ac orci phasellus egestas tellus rutrum tellus. Dolor sit amet consectetur adipiscing elit ut aliquam purus. Quis lectus nulla at volutpat diam ut venenatis. Nibh cras pulvinar mattis nunc sed blandit libero volutpat.
+                    <div className="my-4"/>
+                    Sed lectus vestibulum mattis ullamcorper velit. Non consectetur a erat nam at. At quis risus sed vulputate. Dui ut ornare lectus sit amet est. Urna porttitor rhoncus dolor purus non enim praesent elementum. Et netus et malesuada fames ac turpis egestas maecenas. Nibh venenatis cras sed felis. Vitae tortor condimentum lacinia quis vel eros donec ac. Morbi tempus iaculis urna id volutpat lacus laoreet. Turpis cursus in hac habitasse platea dictumst. Pellentesque id nibh tortor id aliquet lectus proin nibh nisl. Diam quam nulla porttitor massa. Malesuada proin libero nunc consequat interdum varius sit amet. Nisl purus in mollis nunc sed. Sollicitudin ac orci phasellus egestas tellus rutrum tellus. Dolor sit amet consectetur adipiscing elit ut aliquam purus. Quis lectus nulla at volutpat diam ut venenatis. Nibh cras pulvinar mattis nunc sed blandit libero volutpat.
+                    
+                    Sed lectus vestibulum mattis ullamcorper velit. Non consectetur a erat nam at. At quis risus sed vulputate. Dui ut ornare lectus sit amet est. Urna porttitor rhoncus dolor purus non enim praesent elementum. Et netus et malesuada fames ac turpis egestas maecenas. Nibh venenatis cras sed felis. Vitae tortor condimentum lacinia quis vel eros donec ac. Morbi tempus iaculis urna id volutpat lacus laoreet. Turpis cursus in hac habitasse platea dictumst. Pellentesque id nibh tortor id aliquet lectus proin nibh nisl. Diam quam nulla porttitor massa. Malesuada proin libero nunc consequat interdum varius sit amet. Nisl purus in mollis nunc sed. Sollicitudin ac orci phasellus egestas tellus rutrum tellus. Dolor sit amet consectetur adipiscing elit ut aliquam purus. Quis lectus nulla at volutpat diam ut venenatis. Nibh cras pulvinar mattis nunc sed blandit libero volutpat.
+                    <div className="my-4"/>
+                    Sed lectus vestibulum mattis ullamcorper velit. Non consectetur a erat nam at. At quis risus sed vulputate. Dui ut ornare lectus sit amet est. Urna porttitor rhoncus dolor purus non enim praesent elementum. Et netus et malesuada fames ac turpis egestas maecenas. Nibh venenatis cras sed felis. Vitae tortor condimentum lacinia quis vel eros donec ac. Morbi tempus iaculis urna id volutpat lacus laoreet. Turpis cursus in hac habitasse platea dictumst. Pellentesque id nibh tortor id aliquet lectus proin nibh nisl. Diam quam nulla porttitor massa. Malesuada proin libero nunc consequat interdum varius sit amet. Nisl purus in mollis nunc sed. Sollicitudin ac orci phasellus egestas tellus rutrum tellus. Dolor sit amet consectetur adipiscing elit ut aliquam purus. Quis lectus nulla at volutpat diam ut venenatis. Nibh cras pulvinar mattis nunc sed blandit libero volutpat.
+                </div>
+                <div className="flex w-[70%] leading-7 ml-[15%] mb-10">
+                    <button className="text-[#6C757D] text-sm border-[3px] mr-2 border-[#E5E5E5] px-3 py-1 rounded-lg">tag1</button>
+                    <button className="text-[#6C757D] text-sm border-[3px] mr-2 border-[#E5E5E5] px-3 py-1 rounded-lg">tag2</button>
+                    <button className="text-[#6C757D] text-sm border-[3px] mr-2 border-[#E5E5E5] px-3 py-1 rounded-lg">tag3</button>
+                </div>
+                <div className="w-[70%] leading-7 ml-[15%] mb-10 border border-gray-300 border-solid"></div>
+                <p className="w-[70%] leading-7 ml-[15%] pb-12 text-black">
+                    By Author
+                    <br />
+                    <span className ="font-thin text-gray-600">Designation</span>
+                </p>
+                <div>
+                    <img className="absolute bottom-0 right-0 z-0" src="/images/assets/revit/bottom.png"/>
+                </div>
+            </div>
+            <Blogbg/>
+        </div>
+            
+    )
+        
+}
--- sustain-sphere/src/app/components/ui/homepage/SSFeatures.tsx
+++ sustain-sphere/src/app/components/ui/homepage/SSFeatures.tsx
@@ -1,5 +1,7 @@
+'use client'
 import exp from "constants";
 import { Inter, Poppins } from "@next/font/google";
+import Link from "next/link";
 const inter = Inter({
     subsets: ['latin'],
     weight: ['400'],
@@ -16,6 +18,7 @@ const Features= () =>{
         <div className="w-full flex gap-x-6 py-8 pt-10 px-10 my-6">
             <div className="flex relative flex-col px-4 py-6 bg-white border-2 border-[#11633821] rounded-xl shadow-md text-black hover:bg-green-500 hover:text-white hover:cursor-pointer group">
                 <div className="p-4 flex-1 md:p-5">
+                <Link href='/revit'>
                     <img className="w-16 h-16 my-4" src="/images/assets/ss1.png" alt="Image Description"/>
                     <h3 style={pops.style} className="text-lg  mb-3">
                     Environmental Management
@@ -24,14 +27,16 @@ const Features= () =>{
 
                     Explore a premier online platform for cutting-edge environmental management solutions. Discover a wealth of resources, tools, and insights to assist your organization in achieving excellence in sustainability and environmental stewardship.
                     </p>
-                    <a href="/">
+                    
                     <img className="absolute bottom-2 py-6" src="/images/assets/arrow-right-line.svg" width={20} height={20}/>
-                    </a>
+                    
+                </Link>
                 </div>
             </div>
 
             <div className="flex relative flex-col px-4 py-6 bg-white border-2 border-[#11633821] rounded-xl shadow-md text-black hover:bg-green-500 hover:text-white hover:cursor-pointer group">
                 <div className="p-4 flex-1 md:p-5">
+                <Link href='/ssfeaturepages/deforestation'>
                     <img className="w-16 h-16 my-4" src="/images/assets/ss2.png" alt="Image Description"/>
                     <h3 style={pops.style} className="text-lg  mb-3">
                         Community Engagement & Partnering
@@ -39,13 +44,15 @@ const Features= () =>{
                     <p style={inter.style} className="mt-1 text-[#666666] group-hover:text-white  font-semibold mb-8">
                         Harness the power of community engagement and volunteering through SustainSphere. Connect organizations with impactful volunteer opportunities, access valuable resources, and strengthen your commitment to social responsibility for a better future.
                     </p>
-                    <a href="/">
-                        <img className="absolute bottom-2 py-6" src="/images/assets/arrow-right-line.svg" width={20} height={20}/>
-                    </a>
+                    
+                    <img className="absolute bottom-2 py-6" src="/images/assets/arrow-right-line.svg" width={20} height={20}/>
+                </Link>
                 </div>
             </div>
+
             <div className="flex relative flex-col px-4 py-6 bg-white border-2 border-[#11633821] rounded-xl shadow-md text-black hover:bg-green-500 hover:text-white hover:cursor-pointer group">
                 <div className="p-4 flex-1 md:p-5">
+                <Link href='/revit'>
                    <img className="w-16 h-16 my-4" src="/images/assets/ss3.png" alt="Image Description"/>
                    <h3 style={pops.style} className="text-lg  mb-3 ">
                     Integrated Reporting Dashboard
@@ -53,14 +60,15 @@ const Features= () =>{
                     <p style={inter.style} className="mt-1 text-[#666666] group-hover:text-white  font-semibold">
                     With SustainSphere's Integrated Reporting Dashboard. Consolidate and visualize your organization's environmental, social, and financial data in one user-friendly platform. Gain valuable insights, track performance, and streamline reporting for a more sustainable future.
                     </p>
-                   <a href="/">
-                       <img className="absolute bottom-2 py-6" src="/images/assets/arrow-right-line.svg" width={20} height={20}/>
-                   </a>
+                   
+                    <img className="absolute bottom-2 py-6" src="/images/assets/arrow-right-line.svg" width={20} height={20}/>
+                </Link>
                 </div>
             </div>
 
             <div className="flex relative flex-col px-4 py-6 bg-white border-2 border-[#11633821] rounded-xl shadow-md text-black hover:bg-green-500 hover:text-white hover:cursor-pointer group">
                 <div className="p-4 flex-1 md:p-5">
+                <Link href='/revit'>
                     <img className="w-16 h-16 my-4" src="/images/assets/ss4.png" alt="Image Description"/>
                     <h3 style={pops.style} className="text-lg  mb-3 ">
                         View All
@@ -68,12 +76,11 @@ const Features= () =>{
                     <p style={inter.style} className="mt-1 text-[#666666] group-hover:text-white  font-semibold">
                         Explore a premier online platform for cutting-edge environmental management solutions. Discover a wealth of resources, tools, and insights to assist your organization in achieving excellence in sustainability and environmental stewardship.
                     </p>
-                    <a href="/">
-                        <img className="absolute bottom-2 py-6" src="/images/assets/arrow-right-line.svg" width={20} height={20}/>
-                    </a>
+                    
+                    <img className="absolute bottom-2 py-6" src="/images/assets/arrow-right-line.svg" width={20} height={20}/>
+                </Link>
                 </div>
-            </div>
-                          
+            </div>           
         </div>
     );
 }
