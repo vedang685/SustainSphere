36c86804218f9bdaedf6c59110bf9714cc71304f
arpittiwari24 <100207940+arpittiwari24@users.noreply.github.com>
2023-09-23 16:10:05 +0530

    Made Changes in the about us section and made the terms and conditions page

--- sustain-sphere/src/app/(webpages)/about/page.tsx
+++ sustain-sphere/src/app/(webpages)/about/page.tsx
@@ -1,3 +1,4 @@
+import Features from "@/app/components/ui/homepage/Features";
 
 export default function Revit(){
     return(
@@ -16,8 +17,8 @@ export default function Revit(){
                             <h2 className="font-bold text-3xl lg:text-4xl text-gray-800 dark:text-gray-200">
                                 Our Mission
                             </h2>
-                            <p className="text-gray-500">
-                            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.                            </p>
+                            <p className="text-gray-500 text-xl pt-2">
+                            At SustainSphere, our mission is to pave the way for a more sustainable and interconnected world. We are committed to empowering enterprises to embrace and monitor Environmental, Social, and Governance (ESG) initiatives seamlessly. By providing innovative solutions, we aim to be the catalyst for change in the corporate world, where sustainability is not just a goal but a way of doing business.                             </p>
                            <div className="pt-6">
                          </div>
                         </div>
@@ -47,8 +48,10 @@ export default function Revit(){
                             <h2 className="font-bold text-3xl lg:text-4xl text-gray-800 dark:text-gray-200">
                                 Our Story
                             </h2>
-                            <p className="text-gray-500">
-                            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.                            </p>
+                            <p className="text-gray-500 text-xl">
+                            SustainSphere emerged from a shared vision among passionate individuals who recognized the urgent need for comprehensive ESG solutions. We understood that sustainability isn't just a buzzword; it's a fundamental shift in how organizations operate, engage with communities, and protect our planet.
+Our journey began with a simple question: How can we make sustainability accessible to enterprises while fostering collaboration among like-minded organizations and individuals who share this vision?
+                            </p>
                            <div className="pt-6">
                          </div>
                         </div>
@@ -73,39 +76,9 @@ export default function Revit(){
     </div>
 
 
-        <div className="max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 lg:pt-0 mx-auto">
-            {/*// <!-- Grid -->*/}
-            <div className="md:grid md:grid-cols-2 md:items-center md:gap-12 xl:gap-32">
-            <div className="mt-5 sm:mt-10 lg:mt-0">
-                    <div className="space-y-6 sm:space-y-8">
-                        {/*// <!-- Title -->*/}
-                        <div className="space-y-2 md:space-y-4">
-                            <h2 className="font-bold text-3xl lg:text-4xl text-gray-800 dark:text-gray-200">
-                               Words from our Founder
-                            </h2>
-                            <blockquote className=" text-lg italic text-gray-700 mb-6">
-          "Focus on your goals , you will get many holes" - Ashish Yadav, CTO and Founder, SustainSphere
-        </blockquote>
-                           <div className="pt-6">
-                         </div>
-                        </div>
-                    </div>
-                </div>
-
-                <div>
-                    <img className=" rounded-full" src="/images/assets/founder.jpg" alt="Image Description"/>
-                </div>
-                {/*// <!-- End Col -->*/}
-
-                {/*// <!-- End Col -->*/}
-            </div>
-            {/*// <!-- End Grid -->*/}
+        <div>
+            <Features />
         </div>
-
-        {/* <blockquote className="max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto text-lg italic text-gray-700 mb-6">
-          "Focus on your goals , you will get many holes" - Ashish Yadav, CTO and Founder, SustainSphere
-        </blockquote> */}
-  
         {/* Add more content as needed */}
       </>
     )
---
+++ sustain-sphere/src/app/(webpages)/terms-conditions/page.tsx
@@ -0,0 +1,21 @@
+
+
+export default function terms () {
+    return (<>
+        <div className="container mx-auto p-6 rounded-lg shadow-lg text-gray-800 ">
+        <h1 className="text-5xl font-semibold mb-6 py-6 text-center">Terms and Conditions</h1>
+        <div className="prose">
+            <p className="py-5 text-xl">The use of this website is subject to the following terms of use:</p>
+            <ol className=" list-disc pl-6 mb-6">
+        <li className="mb-2 py-3">Acceptance of Terms - By accessing and using the SustainSphere website, you agree to comply with and be bound by these Terms and Conditions of Use.</li>
+        <li className="mb-2 py-3">Use of the Website - You agree not to use the Website for any unlawful or prohibited purpose.</li>
+        <li className="mb-2 py-3">Privacy Policy - Your use of the Website is also governed by our Privacy Policy. By using the Website, you consent to the practices outlined in the Privacy Policy.</li>
+        <li className="mb-2 py-3">User Accounts - You are responsible for maintaining the confidentiality of your account information. You agree to notify us immediately of any unauthorized use of your account.</li>
+        <li className="mb-2 py-3">Changes to Terms and Conditions - SustainSphere reserves the right to modify these Terms and Conditions at any time. It is your responsibility to review these terms periodically.</li>
+        <li className="mb-2 py-3">Contact Information - If you have any questions or concerns regarding these Terms and Conditions, please contact us.</li>
+    </ol>
+        </div>
+    </div>
+        </>
+    )
+}
